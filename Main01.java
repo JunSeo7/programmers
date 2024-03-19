7eb6944fd40ea61993c904596bd5136484bed9a6
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-03-19 17:38:00 +0900

    프로그래머스 1단계

---
+++ Main01.java
@@ -0,0 +1,24 @@
+package kh.model.vo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main30 {
+
+	// 프로그래머스 LV1 - 12954
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int n = sc.nextInt();
+
+		long[] answer = new long[n];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = (long) x * (1 + i);
+		}
+		System.out.println(Arrays.toString(answer));
+
+	}
+
+}
