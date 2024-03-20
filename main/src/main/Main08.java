c14013b32244fe001cfa0e00915fd61bd88ed9a9
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-03-20 23:54:03 +0900

    Lv.1 - 12928
    정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.

---
+++ main/src/main/Main08.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int answer = 0;
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				answer += i;
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
