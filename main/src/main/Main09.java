7a74419535ba0fbdfe347a590f5c2a93e595d288
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-03-20 23:57:03 +0900

    Lv.1 - 12925
    문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
    (잘못된 값이 입력 되는 경우는 없음.)

---
+++ main/src/main/Main09.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main09 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int answer = 0;
+		System.out.print("문자열 입력 : ");
+		String s = sc.nextLine();
+
+		answer = Integer.parseInt(s);
+		System.out.println(answer);
+
+	}
+
+}
