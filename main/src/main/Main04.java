c69932bc6c04233dbd4261c49a95173f841d7346
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-03-20 23:36:46 +0900

    프로그래머스 1단계

---
+++ main/src/main/Main04.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int n = sc.nextInt();
+		long[] answer = new long[n];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = (long) x * (1 + i);
+		}
+		System.out.println(Arrays.toString(answer));
+
+	}
+
+}
