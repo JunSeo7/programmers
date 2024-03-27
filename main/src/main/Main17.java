bff92f2296f3e1cd95516581cb1e09a6073e60cf
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-03-27 10:44:01 +0900

    Lv.1 - 12910
    나누어 떨어지는 숫자 배열

--- main/src/main/Main17.java
+++ main/src/main/Main17.java
@@ -1,28 +1,61 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main17 {
 
+	// 좋은 풀이
 	public int[] solution(int[] arr, int divisor) {
-		int cnt = 0;
-		int[] answer = new int[arr.length];
-		
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] % divisor == 0) {
-				answer[cnt] = arr[i];
-				cnt++;
-			} 
+		Arrays.sort(arr);
+		List<Integer> lst1 = new ArrayList<Integer>();
+
+		for (int item : arr) {
+			if (item % divisor == 0) {
+				lst1.add(item);
+			}
+		}
+
+		int lstSize = lst1.size();
+		if (lstSize == 0) {
+			int[] answer = { -1 };
+			return answer;
+		}
+
+		int[] answer = new int[lstSize];
+		for (int i = 0; i < lstSize; ++i) {
+			answer[i] = lst1.get(i);
 		}
 		System.out.println(Arrays.toString(answer));
-		
 		return answer;
 	}
 
+	// 내 풀이
+	public ArrayList<Integer> solution1(int[] arr, int divisor) {
+
+		ArrayList<Integer> b = new ArrayList<Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % divisor == 0) {
+				b.add(arr[i]);
+			}
+
+		}
+		if (b.size() == 0) {
+			b.add(-1);
+		}
+		b.sort(null);
+		System.out.println(b);
+
+		return b;
+	}
+
+	// 코드 테스트
 	public static void main(String[] args) {
 		Main17 m = new Main17();
-		int[] arr = { 5, 9, 7, 10 };
-		m.solution(arr, 5);
+		int[] arr = { 2, 36, 1, 3 };
+		m.solution(arr, 1);
 	}
 
 }
