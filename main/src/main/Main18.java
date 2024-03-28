565bec5d6991dad3a23ab3d7e16bca0f6a62dfe8
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-03-28 10:11:59 +0900

    Lv. 1 - 12918
    문자열 다루기 기본

--- main/src/main/Main18.java
+++ main/src/main/Main18.java
@@ -1,40 +1,26 @@
 package main;
 
-import java.util.Scanner;
-
 public class Main18 {
 
-	public static final Scanner SC = new Scanner(System.in);
-
 	public boolean solution(String s) {
+
 		boolean answer = true;
-		if (s.length() != 4 || s.length() != 6) {
-			answer = false;
-			System.out.println(answer);
+
+		if (s.length() == 4 || s.length() == 6) {
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) < 48 || s.charAt(i) > 57) {
+					return false;
+				}
+			}
+		} else {
 			return false;
-		}
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) < '0' || s.charAt(i) > '9') {
-				System.out.println(s.charAt(i));
-				answer = false;
-				System.out.println(answer);
-				return false;
-			} 
 		}
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		Main18 m = new Main18();
-		m.solution("1234");
-
-//		PracticeController pc = new PracticeController();
-//		try {
-//			pc.printMenu();
-//		} catch (Exception e) {
-//			System.out.println("오류 발생");
-//			e.printStackTrace();
-//		}
+		m.solution("a234");
 
 	}
 }
